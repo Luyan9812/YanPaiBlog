5a66ea7c01a79d1881cfcf478912d288bb97e835
SupperMonster <luyan9812@163.com>
2024-09-12 09:31:00 +0800

    服务器配置

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,5 @@
 const tokenStr = 'token';
-const baseURL = 'http://127.0.0.1:8081';
+// const baseURL = 'http://127.0.0.1:8081';
+const baseURL = 'http://47.96.95.64:8080';
 
 export { tokenStr, baseURL };
